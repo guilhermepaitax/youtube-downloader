e6a2c308a381a254b2f5059c95c687191d2e8431
guilhermepaitax <guilhermepaitax16@gmail.com>
2020-01-16 11:00:18 -0300

    creating structure

---
+++ backend/src/app.js
@@ -0,0 +1,19 @@
+import './bootstrap';
+
+import express from 'express';
+import cors from 'cors';
+
+class App {
+  constructor() {
+    this.server = express();
+
+    this.middlewares();
+  }
+
+  middlewares() {
+    this.server.use(cors());
+    this.server.use(express.json());
+  }
+}
+
+export default new App().server;
